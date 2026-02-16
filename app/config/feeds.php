bfbfa66c69747383e5733bfbe28d5345b252546e
Trollkopf <max.serratosa@gmail.com>
2026-02-16 09:51:51 +0100

    updated

--- app/config/feeds.php
+++ app/config/feeds.php
@@ -1,37 +1,15 @@
 <?php
 
 return [
-
-    // MLS COSTA
     'https://www.propmls.com/property-export/26v567a0143/all.xml',
-
-    // COMASKEY
     'https://comaskeys.us15.list-manage.com/track/click?u=a42abd6990d2d79ea01d06f5e&id=f18cde45a1&e=e500bad6dc',
-
-    // UNITED REAL ESTATE
     'http://procesos.apinmo.com/xml/v2/wI6wsbBM/6689-web.xml',
-
-    // MOVR
     'https://movr.es/xml/kyero.php?f=681c8a8a276a0',
-
-    // BENNECKE
     'https://api.bennecke.com/xml/kyero-reventa-multi',
-
-    // HOME PANDORA
     'https://procesos.apinmo.com/portal/kyeroagencias3/6225-kyero-7AHgO55n.xml',
-
-    // CASTILLO HOMES
     'https://castillohomes.se/xml/kyero.php?f=64ae6429d422f',
-
-    // SUNRISA REAL ESTATE
     'https://members.alphashare.com/_ULURJcxjjIUf.xml',
-
-    // LUXURY INVESTMENTS
     'https://www.luxuryinvestments.eu/wp-content/plugins/pluginXmlKyero/exportadorKyero/kyeroSegundaMano.xml',
-
-    // COSTA IBERICA
     'https://costaiberica.com/xml/kyero.php?f=672debdfb4a49',
-
-    // TPPCM
     'https://tppcm.com/xml/mls.php?u=di9ienMzNE8wNlZycklHdDllQXRXQT09',
 ];
